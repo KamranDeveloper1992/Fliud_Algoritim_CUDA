a0251073205e63416a61b405a9159cd668536e91
KamranDeveloper1992 <kamran6806624545@gmail.com>
2025-05-09 21:49:37 +0300

    Fliud_Algoritim_CUDA
    Fliud_Algoritim_CUDA

---
+++ FliudAlgoritimCUDA/kernel.cu
@@ -0,0 +1,28 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void SimulationFliud(int sizeFluid, int rateFluid, int* status) {
+    if (sizeFluid > 50 || rateFluid < 20) {
+        *status = 1;  
+    }
+    else {
+        *status = 0; 
+    }
+}
+int main() {
+    int result = -1;
+    int* d_result;
+
+    cudaMalloc((void**)&d_result, sizeof(int));
+
+
+    SimulationFliud << <1, 1 >> > (100, 200, d_result);
+
+
+    cudaFree(d_result);
+
+    cudaDeviceReset();
+
+    return 0;
+}
